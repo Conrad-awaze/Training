60a7a02d53be75da18156843e709a56208744bfb
Conrad <conrad.gauntlett@awaze.com>
2022-09-09 17:44:24 +0100

    First commit

---
+++ AWSS3List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client("", "");
+
+Console.WriteLine("AWS S3 Bucket Lister" + Environment.NewLine);
+
+var listResponse = await MyListBucketAsync(s3Client);
+Console.WriteLine($"Number of buckets: {listResponse.Buckets.Count}");
+
+foreach (S3Bucket b in listResponse.Buckets)
+{
+    Console.WriteLine($"BucketName: {b.BucketName}");
+    
+}
+
+static async Task<ListBucketsResponse> MyListBucketAsync(IAmazonS3 s3Client)
+{
+    return await s3Client.ListBucketsAsync();
+}
+Console.ReadLine();
